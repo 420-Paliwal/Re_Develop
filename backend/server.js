a640b24f918ac79b00785ef1107782edd19c362b
420-Paliwal <harshpaliwal0420@gmail.com>
2026-03-31 16:44:37 +0530

    add jwt

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,19 @@ app.get("/", (req, res) => {
     res.send("server running")
 })
 
+function auth(req, res, next){
+    const token = req.headers.authorization;
+
+    if(!token) return res.send("No token");
+    try{
+        const decoded = jwt.verify(token, "secretkey");
+        req.user = decoded,
+        next();
+    }catch(err){
+        res.send("Invalid token");
+    }
+}
+
 app.post("/login", (req, res)=>{
     const { email, password } = req.body;
     if (email == "test@gmail.com" && password == "1234"){
@@ -19,11 +32,16 @@ app.post("/login", (req, res)=>{
             {expiresIn: "1h"}
         )
         res.json({success: true, message: "Login successful", token}); 
+        console.log("success")
     }
     else{
         res.json({success: false, message: "Invalid credentials"})
     }
 });
+
+app.get('/profile', auth, (req, res) => {
+    res.json({message: "Welcome", user: req.user})
+})
 
 app.listen(5000, ()=>{
     console.log("server started on port 5000")
